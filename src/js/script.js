610dfe585f5eae06b6600fc32e49326f9546fd32
Sergey <sergeymerlov0@gmail.com>
2021-06-16 20:07:26 +0300

    second

--- src/js/script.js
+++ src/js/script.js
@@ -1,90 +1,36 @@
-$(document).ready(function(){
-    $('.carusel__inner').slick({
-        speed: 1200,
-        adaptiveHeight: false,
-        dotsClass: 'slick-dots',
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow/right.png"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false,
-                
-                    
-            }
-            }
-        ]
-    });
+window.addEventListener('DOMContentLoaded', () => {
+    const contans = document.querySelectorAll('.block__text'),
+          tabs = document.querySelectorAll('.block__tab'),
+          tabPerent = document.querySelector('.block__tabs');
 
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-        $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-      });
+    function removeTabContent() {
+        contans.forEach(item => {
+            item.style.display = 'none';
+        });
+        tabs.forEach(item => {
+            item.classList.remove('block__tab_activ');
+        });
+    }
 
-      $('.catalog-item__link').each(function(i) {
-          $(this).on('click', function(e) {
-              e.preventDefault();
-              $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-              $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-          })
-      })
+    function showTabContent(i = 0) {
+        contans[i].style.display = 'block';
+        tabs[i].classList.add('block__tab_activ');
 
-      $('.catalog-item__back').each(function(i) {
-        $(this).on('click', function(e) {
-            e.preventDefault();
-            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-        })
-    })
+    }
 
-    //modal
+    removeTabContent();
+    showTabContent();
 
-    $('[data-modal=consultacion]').on('click', function() {
-        $('.overlay, #consultacion').fadeIn('slow');
-    });
-    $('.modal__close').on('click', function() {
-        $('.overlay, #consultacion, #thanks, #order').fadeOut('slow');
-    });
-    $('.button_mini').on('click', function() {
-        $('.overlay, #order').fadeIn('slow');
-    });
-    $('.button_mini').each(function(i) {
-        $(this).on('click', function() {
-            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-            $('.overlay, #order').fadeIn('slow');
-        })
-    });
+    tabPerent.addEventListener('click', (event) => {
+        const target = event.target;
 
-    $('.#order form').validate();
-    $('#consultacion form').validate({
-        rules: {
-            name: "required",
-            phone: "required",
-            email: {
-                required: true,
-                email: true
-            }
+        if(target && target.classList.contains('block__tab')) {
+            tabs.forEach((item, i) => {
+                if(target == item) {
+                    removeTabContent();
+                    showTabContent(i);
+                }
+            });
         }
     });
-    $('#consultacion-form').validate();
-
-    $('form').submit(function(e) {
-        e.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: "../mailer/smart.php",
-            data: $(this).serialize()
-        }).done(function() {
-            $(this).find("input").val("");
-            $('#consultation, #order').fadeOut();
-            $('.overlay, #thanks').fadeIn('slow');
-
-            $('form').trigger('reset');
-        });
-        return false;
-    });
-    
-  });
+});
